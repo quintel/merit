d22a46abdff66ffbf2fd64a1af04d0e4216a45c7
Chael Kruip <chael.kruip@quintel.com>
2012-11-30 15:57:07 +0100

    Fixed typo in stub1.rb

--- examples/stub1.rb
+++ examples/stub1.rb
@@ -12,8 +12,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.98,
         fixed_costs_per_unit: 4236515.36,
-        fixed_om_costs: 477000,
-        load_profile: :solar_pv,
+        fixed_om_costs_per_unit: 477000,
+        load_profile_key: :solar_pv,
         full_load_hours: 1050
       )
     )
@@ -26,8 +26,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.99,
         fixed_costs_per_unit: 15129166.33,
-        fixed_om_costs: 400000,
-        load_profile: :solar_pv,
+        fixed_om_costs_per_unit: 400000,
+        load_profile_key: :solar_pv,
         full_load_hours: 500
       )
     )
@@ -40,8 +40,8 @@ module Merit
         number_of_units: 360.0,
         availability: 0.95,
         fixed_costs_per_unit: 531768.45,
-        fixed_om_costs: 147579.9,
-        load_profile: :wind_inland,
+        fixed_om_costs_per_unit: 147579.9,
+        load_profile_key: :wind_inland,
         full_load_hours: 2500
       )
     )
@@ -54,8 +54,8 @@ module Merit
         number_of_units: 66.66666667,
         availability: 0.95,
         fixed_costs_per_unit: 531768.45,
-        fixed_om_costs: 147579.9,
-        load_profile: :wind_coastal,
+        fixed_om_costs_per_unit: 147579.9,
+        load_profile_key: :wind_coastal,
         full_load_hours: 3000
       )
     )
@@ -68,8 +68,8 @@ module Merit
         number_of_units: 64.76190476,
         availability: 0.92,
         fixed_costs_per_unit: 1643536.011,
-        fixed_om_costs: 428882.8856,
-        load_profile: :wind_offshore,
+        fixed_om_costs_per_unit: 428882.8856,
+        load_profile_key: :wind_offshore,
         full_load_hours: 3500
       )
     )
@@ -82,8 +82,8 @@ module Merit
         number_of_units: 2547.332186,
         availability: 0.98,
         fixed_costs_per_unit: 2545.292412,
-        fixed_om_costs: 357.75,
-        load_profile: :solar_pv,
+        fixed_om_costs_per_unit: 357.75,
+        load_profile_key: :solar_pv,
         full_load_hours: 1050
       )
     )
@@ -96,8 +96,8 @@ module Merit
         number_of_units: 51023.14018,
         availability: 0.98,
         fixed_costs_per_unit: 222.9245208,
-        fixed_om_costs: 35.775,
-        load_profile: :solar_pv,
+        fixed_om_costs_per_unit: 35.775,
+        load_profile_key: :solar_pv,
         full_load_hours: 1050
       )
     )
@@ -110,8 +110,8 @@ module Merit
         number_of_units: 122.0877551,
         availability: 0.97,
         fixed_costs_per_unit: 2543878.235,
-        fixed_om_costs: 0,
-        load_profile: :industry_chp,
+        fixed_om_costs_per_unit: 0,
+        load_profile_key: :industry_chp,
         full_load_hours: 5442.834138
       )
     )
@@ -124,8 +124,8 @@ module Merit
         number_of_units: 10.54725,
         availability: 0.97,
         fixed_costs_per_unit: 9479267.598,
-        fixed_om_costs: 2913267.598,
-        load_profile: :industry_chp,
+        fixed_om_costs_per_unit: 2913267.598,
+        load_profile_key: :industry_chp,
         full_load_hours: 5247.813411
       )
     )
@@ -138,8 +138,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 4974342.555,
-        fixed_om_costs: 1301009.555,
-        load_profile: :industry_chp,
+        fixed_om_costs_per_unit: 1301009.555,
+        load_profile_key: :industry_chp,
         full_load_hours: 4204.8
       )
     )
@@ -152,8 +152,8 @@ module Merit
         number_of_units: 11.68978647,
         availability: 0.9,
         fixed_costs_per_unit: 26877150,
-        fixed_om_costs: 0,
-        load_profile: :industry_chp,
+        fixed_om_costs_per_unit: 0,
+        load_profile_key: :industry_chp,
         full_load_hours: 6190.47619
       )
     )
@@ -166,8 +166,8 @@ module Merit
         number_of_units: 3023.581081,
         availability: 0.97,
         fixed_costs_per_unit: 116478.4738,
-        fixed_om_costs: 13062.47379,
-        load_profile: :agriculture_chp,
+        fixed_om_costs_per_unit: 13062.47379,
+        load_profile_key: :agriculture_chp,
         full_load_hours: 3980.424144
       )
     )
@@ -180,8 +180,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 18359173.13,
-        fixed_om_costs: 0,
-        load_profile: :buildings_chp,
+        fixed_om_costs_per_unit: 0,
+        load_profile_key: :buildings_chp,
         full_load_hours: 6097.777778
       )
     )
@@ -194,8 +194,8 @@ module Merit
         number_of_units: 871.7816937,
         availability: 0.97,
         fixed_costs_per_unit: 49847.77778,
-        fixed_om_costs: 0,
-        load_profile: :buildings_chp,
+        fixed_om_costs_per_unit: 0,
+        load_profile_key: :buildings_chp,
         full_load_hours: 3942
       )
     )
@@ -208,8 +208,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 18359173.13,
-        fixed_om_costs: 0,
-        load_profile: :buildings_chp,
+        fixed_om_costs_per_unit: 0,
+        load_profile_key: :buildings_chp,
         full_load_hours: 6097.777778
       )
     )
@@ -222,8 +222,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 49847.77778,
-        fixed_om_costs: 0,
-        load_profile: :buildings_chp,
+        fixed_om_costs_per_unit: 0,
+        load_profile_key: :buildings_chp,
         full_load_hours: 3942
       )
     )
@@ -236,8 +236,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 2453,
-        fixed_om_costs: 200,
-        load_profile: :buildings_chp,
+        fixed_om_costs_per_unit: 200,
+        load_profile_key: :buildings_chp,
         full_load_hours: 0
       )
     )
@@ -250,8 +250,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 54068.71357,
-        fixed_om_costs: 6056.838239,
-        load_profile: :buildings_chp,
+        fixed_om_costs_per_unit: 6056.838239,
+        load_profile_key: :buildings_chp,
         full_load_hours: 4000
       )
     )
@@ -264,8 +264,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 933,
-        fixed_om_costs: 110,
-        load_profile: :buildings_chp,
+        fixed_om_costs_per_unit: 110,
+        load_profile_key: :buildings_chp,
         full_load_hours: 0
       )
     )
@@ -278,8 +278,8 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 933,
-        fixed_om_costs: 110,
-        load_profile: :buildings_chp,
+        fixed_om_costs_per_unit: 110,
+        load_profile_key: :buildings_chp,
         full_load_hours: 0
       )
     )
@@ -292,7 +292,7 @@ module Merit
         number_of_units: 5.749536178,
         availability: 0.9,
         fixed_costs_per_unit: 61526416,
-        fixed_om_costs: 9066666.667,
+        fixed_om_costs_per_unit: 9066666.667,
       )
     )
 
@@ -304,7 +304,7 @@ module Merit
         number_of_units: 2.365705626,
         availability: 0.88,
         fixed_costs_per_unit: 111231290.8,
-        fixed_om_costs: 16000000,
+        fixed_om_costs_per_unit: 16000000,
       )
     )
 
@@ -316,7 +316,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.89,
         fixed_costs_per_unit: 49359621.7,
-        fixed_om_costs: 15059622.37,
+        fixed_om_costs_per_unit: 15059622.37,
       )
     )
 
@@ -328,7 +328,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.97,
         fixed_costs_per_unit: 92837254.57,
-        fixed_om_costs: 3670588.235,
+        fixed_om_costs_per_unit: 3670588.235,
       )
     )
 
@@ -340,7 +340,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.89,
         fixed_costs_per_unit: 27886950.9,
-        fixed_om_costs: 9527777.778,
+        fixed_om_costs_per_unit: 9527777.778,
       )
     )
 
@@ -352,7 +352,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.87,
         fixed_costs_per_unit: 178999174,
-        fixed_om_costs: 21373442.62,
+        fixed_om_costs_per_unit: 21373442.62,
       )
     )
 
@@ -364,7 +364,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.85,
         fixed_costs_per_unit: 76566794.3,
-        fixed_om_costs: 13063555.56,
+        fixed_om_costs_per_unit: 13063555.56,
       )
     )
 
@@ -376,7 +376,7 @@ module Merit
         number_of_units: 0.322704082,
         availability: 0.9,
         fixed_costs_per_unit: 157393563.3,
-        fixed_om_costs: 16000000,
+        fixed_om_costs_per_unit: 16000000,
       )
     )
 
@@ -388,7 +388,7 @@ module Merit
         number_of_units: 5.104591837,
         availability: 0.9,
         fixed_costs_per_unit: 61526416,
-        fixed_om_costs: 9066666.667,
+        fixed_om_costs_per_unit: 9066666.667,
       )
     )
 
@@ -400,7 +400,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.89,
         fixed_costs_per_unit: 49359621.7,
-        fixed_om_costs: 15059622.37,
+        fixed_om_costs_per_unit: 15059622.37,
       )
     )
 
@@ -412,7 +412,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.85,
         fixed_costs_per_unit: 317248000,
-        fixed_om_costs: 42048000,
+        fixed_om_costs_per_unit: 42048000,
       )
     )
 
@@ -424,7 +424,7 @@ module Merit
         number_of_units: 0.31875,
         availability: 0.9,
         fixed_costs_per_unit: 577551594.8,
-        fixed_om_costs: 80901594.77,
+        fixed_om_costs_per_unit: 80901594.77,
       )
     )
 
@@ -436,7 +436,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.89,
         fixed_costs_per_unit: 79981066.11,
-        fixed_om_costs: 19414400,
+        fixed_om_costs_per_unit: 19414400,
       )
     )
 
@@ -448,7 +448,7 @@ module Merit
         number_of_units: 1.442176871,
         availability: 0.89,
         fixed_costs_per_unit: 6634766.741,
-        fixed_om_costs: 927202.5,
+        fixed_om_costs_per_unit: 927202.5,
       )
     )
 
@@ -460,7 +460,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.85,
         fixed_costs_per_unit: 137557916.7,
-        fixed_om_costs: 15872786.89,
+        fixed_om_costs_per_unit: 15872786.89,
       )
     )
 
@@ -472,7 +472,7 @@ module Merit
         number_of_units: 3.391901654,
         availability: 0.88,
         fixed_costs_per_unit: 111231290.8,
-        fixed_om_costs: 16000000,
+        fixed_om_costs_per_unit: 16000000,
       )
     )
 
@@ -484,7 +484,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.89,
         fixed_costs_per_unit: 49359621.7,
-        fixed_om_costs: 15059622.37,
+        fixed_om_costs_per_unit: 15059622.37,
       )
     )
 
@@ -496,7 +496,7 @@ module Merit
         number_of_units: 4.828282828,
         availability: 0.89,
         fixed_costs_per_unit: 29085600,
-        fixed_om_costs: 1365600,
+        fixed_om_costs_per_unit: 1365600,
       )
     )
 
@@ -508,7 +508,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.89,
         fixed_costs_per_unit: 112037799.7,
-        fixed_om_costs: 19607800,
+        fixed_om_costs_per_unit: 19607800,
       )
     )
 
@@ -520,7 +520,7 @@ module Merit
         number_of_units: 0.0,
         availability: 0.85,
         fixed_costs_per_unit: 240498185.1,
-        fixed_om_costs: 21906185.09,
+        fixed_om_costs_per_unit: 21906185.09,
       )
     )
 
