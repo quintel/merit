2433b4764cae16c284a64ea85a5608766d33f630
Nikolay Petrachkov <nikolay.petrachkov@quintel.com>
2013-03-22 11:14:38 +0100

    Add Storage#utilization and Storage#available_capacity

--- lib/merit/participants/storage.rb
+++ lib/merit/participants/storage.rb
@@ -1,18 +1,37 @@
 module Merit
-  
+
+  # The Storage within the Merit order is responsible for storing excess
+  # of electricity production and for producing electricity if
+  # demand is higher that the amount being produced by must-runs and
+  # volatile producers.
   class Storage < Participant
 
-    attr_accessor :capacity, :max_input, :max_output
+    attr_reader :capacity, :max_input, :max_output
+    attr_accessor :utilization
 
+    # Public: creates a new storage
+    # params opts[Hash] set the attributes
+    # returns Storage
+    # capacity - is a maximum energy that can be stored in the storage
+    # max_input - maximum energy that can be transfered into the storage in a time
+    #             unit
+    # max_output - maximum energy that can be retrieved from the storage in a time
+    #              unit
+    # utilization - amount of energy currently stored in the storage
     def initialize(opts)
       super
       require_attributes :capacity,
                          :max_input,
                          :max_output
 
-      @capacity   = opts[:capacity]
-      @max_input  = opts[:max_input]
-      @max_output = opts[:max_output]
+      @capacity    = opts[:capacity]
+      @max_input   = opts[:max_input]
+      @max_output  = opts[:max_output]
+      @utilization = opts[:utilization] || 0
+    end
+
+    def available_capacity
+      capacity - utilization
     end
   end
 end
--- spec/merit/participants/storage_spec.rb
+++ spec/merit/participants/storage_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 module Merit
-  
+
   describe Storage do
 
     let(:storage) do
@@ -9,7 +9,8 @@ module Merit
         key: :foo,
         capacity: 100,
         max_input: 40,
-        max_output: 50
+        max_output: 50,
+        utilization: 60
       )
     end
 
@@ -18,17 +19,28 @@ module Merit
     it { should respond_to(:capacity) }
     it { should respond_to(:max_input) }
     it { should respond_to(:max_output) }
+    it { should respond_to(:utilization) }
 
     describe "#new" do
       it "remembers more attributes than basic participants" do
         expect(storage.key).to eql(:foo)
         expect(storage.max_input).to eql(40)
         expect(storage.max_output).to eql(50)
+        expect(storage.capacity).to eql(100)
+        expect(storage.utilization).to eql(60)
+      end
+
+      it "has default utilization of 0" do
+        storage = Storage.new(key: :test, capacity: 100, max_input: 20,
+                              max_output: 10)
+        expect(storage.utilization).to eql 0
       end
     end
 
-    describe "subject" do
-      
+    describe "#available_capacity" do
+      it "returns the difference between capacity and utilization" do
+        expect(storage.available_capacity).to eql 40
+      end
     end
   end # describe Storage
 end #module Merit
