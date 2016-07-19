4aede408dd418e814dfc475fa50b14fe7dfa0a5a
Gerard Westerhof <gerard@grdw.nl>
2016-07-19 09:29:30 +0200

    Adding total number of excess events

--- lib/merit/excess.rb
+++ lib/merit/excess.rb
@@ -2,22 +2,27 @@ module Merit
   class Excess
     include NetLoadHelper
 
-    # Public: event_groups
+    # Public: Determines the total number of excess events.
     #
-    # Determines a chain of events by calling +number_of_events+ with
+    # Returns an integer.
+    def total_number_of_events
+      net_load.count(&over_producing)
+    end
+
+    # Public: Determines a chain of events by calling +number_of_events+ with
     # a range of durations.
-    #
     def event_groups(durations = [])
       durations.map do |duration|
         [ duration, number_of_events(duration) ]
       end
     end
 
-    # Public: number_of_events
+    # Public: Determines the amount of times production exceeds consumption
+    # by the specified duration.
     #
-    # Given a duration in hours, this method determines the amount of times
-    # production exceeds consumption by the specified duration.
+    # duration - Length of event
     #
+    # Returns an integer
     def number_of_events(duration)
       events.inject(0) do |result, (_, streak)|
         result + (streak.size >= duration ? 1 : 0)
@@ -26,8 +31,12 @@ module Merit
 
     private
 
+    def over_producing
+      -> (point) { point > 1e-10 }
+    end
+
     def events
-      @events ||= net_load.values.chunk { |point| point > 1e-10 }.select(&:first)
+      @events ||= net_load.values.chunk(&over_producing).select(&:first)
     end
   end
 end
