76f23430d584e27f023f4ab635c3a3691a291709
noracato <ncschinkel@gmail.com>
2024-06-26 11:37:47 +0200

    Add optional constraint to Flex::WithSatisfiedDemand

--- lib/merit/flex/with_satisfied_demand.rb
+++ lib/merit/flex/with_satisfied_demand.rb
@@ -4,12 +4,14 @@ module Merit
   module Flex
     # Flex participant with demand partially satisfied by a given satisfied_demand_curve.
     # The direct satisfied demand is not price setting.
+    # Optionally, takes a constraint lambda.
     #
     # A curve with negative values represents satisfied input.
     class WithSatisfiedDemand < Base
       def initialize(opts)
         super
 
+        @constraint = opts[:constraint] || ->(_, a) { a }
         @load_curve = Curve.new(opts[:satisfied_demand_curve])
         @price_setting = Array.new(8760, false)
       end
@@ -19,11 +21,15 @@ module Merit
       end
 
       def assign_excess(point, amount)
-        assigned = super
+        input_cap = @input_capacity + @load_curve.get(point)
+        amount = [amount, input_cap].min
+        amount = @constraint.call(point, amount)
 
-        @price_setting[point] = true unless assigned.zero?
+        @load_curve.set(point, @load_curve.get(point) - amount)
 
-        assigned
+        @price_setting[point] = true unless amount.zero?
+
+        amount
       end
     end
   end
