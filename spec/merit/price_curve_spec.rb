7b7a3cf1b27eaf2f50d1caa14f5f3ceb9ea99809
noracato <ncschinkel@gmail.com>
2025-03-27 16:21:07 +0100

    Remove Marginal Marker and inflexible price setting behaviour

--- spec/merit/price_curve_spec.rb
+++ spec/merit/price_curve_spec.rb
@@ -405,15 +405,7 @@ end
 
 # Tests price curves with always-on consumers and producers. Sorry for the mess. :(
 describe Merit::PriceCurve do
-  context 'with priced always-on production' do
-    let(:inflexible_consumer) do
-      FactoryBot.build(:user_with_curve, load_curve: [4.0, 4.0])
-    end
-
-    let(:inflexible_producer) do
-      FactoryBot.build(:curve_producer, load_curve: [3.0, 2.0])
-    end
-
+  context 'with an optimised storage producer' do
     let(:storage_producer) do
       FactoryBot.build(:optimizing_storage_producer, load_curve: [1.0, 1.0])
     end
@@ -432,8 +424,6 @@ describe Merit::PriceCurve do
     let(:curve) do
       order = Merit::Order.new
 
-      order.participants.add(inflexible_consumer)
-      order.participants.add(inflexible_producer)
       order.participants.add(storage_producer)
       order.participants.add(dispatchable_producer)
 
@@ -442,15 +432,13 @@ describe Merit::PriceCurve do
 
     # In hour one we have:
     #
-    # * Inflexible consumption of 4.0.
-    # * Inflexible production of 3.0.
     # * Storage production of 1.0.
     # * Dispatchable production of 0.0.
     #
-    # Storage and inflexible production meet demand without the dispatchable running.
-    context 'when the priced producer runs and no dispatchable does' do
-      it 'calculates a price based on the first unused dispatchable' do
-        expect(curve.get(0)).to eq(19.99)
+    # Storage meets demand without the dispatchable running.
+    context 'when the storage producer runs and no dispatchable does' do
+      it 'creates a surplus' do
+        expect(curve.get(0)).to eq(0)
       end
     end
 
@@ -461,11 +449,7 @@ describe Merit::PriceCurve do
     end
   end
 
-  context 'with priced always-on consumption' do
-    let(:inflexible_consumer) do
-      FactoryBot.build(:user_with_curve, load_curve: [5.0, 5.0, 3.0, 4.0])
-    end
-
+  context 'with an optimised storage consumer' do
     let(:storage_consumer) do
       FactoryBot.build(:optimizing_storage_consumer, load_curve: [1.0, 1.0, 1.0, 1.0])
     end
@@ -477,10 +461,6 @@ describe Merit::PriceCurve do
         flex.assign_excess(2, 2.0)
         flex.assign_excess(3, 0.0)
       end
-    end
-
-    let(:inflexible_producer) do
-      FactoryBot.build(:curve_producer, load_curve: [3.0, 5.0, 5.0, 4.0])
     end
 
     let(:nuclear_producer) do
@@ -504,10 +484,8 @@ describe Merit::PriceCurve do
     let(:curve) do
       order = Merit::Order.new
 
-      order.participants.add(inflexible_consumer)
       order.participants.add(storage_consumer)
       order.participants.add(flexible_consumer)
-      order.participants.add(inflexible_producer)
       order.participants.add(nuclear_producer)
       order.participants.add(gas_producer)
 
@@ -516,16 +494,14 @@ describe Merit::PriceCurve do
 
     # In hour one we have:
     #
-    # * Inflexible consumption of 5.0.
     # * Storage consumption of 1.0.
     # * Flexible consumption of 0.0.
-    # * Inflexible production of 3.0.
     # * Nuclear production of 1.0.
     # * Gas production of 2.0.
     #
-    # The gas producer runs to meet demand of the storage consumer, but no inflexible consumption
-    # occurs. The price is set by the expensive gas producer.
-    describe 'when a more expensive dispatchable meets priced inflexible demand' do
+    # The gas producer runs to meet demand of the storage consumer.
+    # The price is set by the expensive gas producer.
+    describe 'when an expensive dispatchable meets priced inflexible demand' do
       it 'takes the price of the dispatchable' do
         expect(curve.get(0)).to eq(40.0)
       end
@@ -533,53 +509,31 @@ describe Merit::PriceCurve do
 
     # In hour two we have:
     #
-    # * Inflexible consumption of 5.0.
     # * Storage consumption of 1.0.
     # * Flexible consumption of 0.0.
-    # * Inflexible production of 5.0.
     # * Nuclear production of 1.0.
     # * Gas production of 0.0.
     #
-    # The inflexible production and nuclear production are both sufficient to meet inflexible demand
-    # and the storage. P2G is willing to pay 30 but the gas producer wants 40. The storage must be
-    # priced at 30.01 so that it is prioritized over P2G while preventing P2G from being consuming.
-    describe 'when a cheaper dispatchable meets priced inflexible demand' do
-      it 'calculates a price based on the first unused flexible demand' do
-        expect(curve.get(1)).to eq(30.01)
+    # The nuclear production is sufficient to meet storage
+    # P2G is willing to pay 30 but the gas producer wants 40.
+    describe 'when a cheaper dispatchable meets demand' do
+      it 'calculates a price based on the nuclear plant' do
+        expect(curve.get(1)).to eq(5.0)
       end
     end
 
     # In hour three we have:
     #
-    # * Inflexible consumption of 3.0.
     # * Storage consumption of 1.0.
     # * Flexible consumption of 2.0.
-    # * Inflexible production of 5.0.
     # * Nuclear production of 1.0.
     # * Gas production of 0.0.
     #
-    # Inflexible consumption and storage are met by inflexible production. Nuclear provides energy to
-    # P2G, with P2G setting the price
+    # Flexible consumption and storage are not met by production. Nuclear provides energy
+    # but not enough to fill the deficit
     describe 'when a dispatchable partially satisfied flexible production' do
-      it 'uses the price of the consumer' do
-        expect(curve.get(2)).to eq(30)
-      end
-    end
-
-    # In hour four we have:
-    #
-    # * Inflexible consumption of 4.0.
-    # * Storage consumption of 1.0.
-    # * Flexible consumption of 0.0.
-    # * Inflexible production of 5.0.
-    # * Nuclear production of 0.0.
-    # * Gas production of 0.0.
-    #
-    # Inflexible production meets inflexible demand and storage. P2G does not run. As in hour 2 the
-    # price must be set by storage to prevent P2G from consuming.
-    describe 'when inflexible production meets inflexible demand' do
-      it 'calculates a price based on the first unused flexible demand' do
-        expect(curve.get(3)).to eq(30.01)
+      it 'uses the price of the deficit' do
+        expect(curve.get(2)).to eq(3000)
       end
     end
   end
