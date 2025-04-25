cf6f6045ca43860b31391034456a3f03845428b3
louispt1 <louisparkestalbot1@gmail.com>
2025-04-25 11:08:08 +0200

    Add methods for peak and volume of blackout hours

--- lib/merit/blackout.rb
+++ lib/merit/blackout.rb
@@ -2,14 +2,30 @@
 
 module Merit
   # Computes the number of hours in which there is insufficient production to
-  # meet demand.
+  # meet demand, plus the volume and peak of those shortfalls.
   class Blackout
+    EPSILON = 1e-5
+
     def initialize(net_load)
       @net_load = net_load
     end
 
+    # Number of hours with a shortfall
     def number_of_hours
-      @net_load.count { |val| val < -1e-5 }
+      @net_load.count { |val| val < -EPSILON }
+    end
+
+    # Total volume of shortfall (sum of all deficits)
+    def volume
+      @net_load
+        .select { |val| val < -EPSILON }    # only deficit hours
+        .sum     { |val| -val }             # flip sign and sum
+    end
+
+    # Peak hourly shortfall (the largest single-hour deficit)
+    def peak
+      worst = @net_load.min                # most negative value
+      worst < -EPSILON ? -worst : 0.0      # if it’s a deficit, flip sign; otherwise return zero
     end
   end
 end
